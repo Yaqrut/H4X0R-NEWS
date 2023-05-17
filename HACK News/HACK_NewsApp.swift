1690f8da93949602c8df0f6c3310d46f8d19342e
Yaqrut <market.brocars@gmail.com>
2023-05-16 18:58:50 -0600

    Initial Commit

---
+++ HACK News/HACK_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HACK_NewsApp.swift
+//  HACK News
+//
+//  Created by YAQRUT on 2023-05-16.
+//
+
+import SwiftUI
+
+@main
+struct HACK_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
